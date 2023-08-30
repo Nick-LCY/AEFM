cde2ef7719a6f970e2d1ef40869a9c92b3c234c5
NickLin <nicklin9907@gmail.com>
2023-08-30 13:18:54 +0800

    docstring of inf generators

--- inf_generator/interfaces.py
+++ inf_generator/interfaces.py
@@ -3,6 +3,9 @@ from models import Node
 
 
 class InfGeneratorInterface(ABC):
+    """Interference generator interface, need to have at least two methods: gene
+    rate and clear."""
+
     @abstractmethod
     def generate(self, count: int, nodes: list[Node], wait: bool) -> None:
         """Generate interference with certain amount."""
